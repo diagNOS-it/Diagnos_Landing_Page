3661a5212ebc98e8ddff1a1459566519beaa1fd0
diagNOS-it <scott@diagnos.it.com>
2025-08-31 12:17:59 +1200

    Create config.js
    Setting up config

---
+++ config.js
@@ -0,0 +1,10 @@
+// Central config for your landing page
+const CONFIG = {
+  companyName: "DiagNOS",
+  email: "scott@diagnos.it.com",
+  domain: "https://www.diagnos.it.com",
+  diagnosprivacyPolicyUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/privacy_policy.html",
+  diagnostermsOfServiceUrl: "https://raw.githubusercontent.com/diagNOS-it/diagNOS_policies/refs/heads/main/policies/terms_policy.html",
+  privacyPolicyUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/Elephnt_Policies@main/policies/privacy_policy.html",
+  mission: "To provide rapid diagnostic assistance offline, where ever you need it."
+};
