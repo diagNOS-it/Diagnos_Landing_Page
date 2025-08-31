fe308b9aeb6f25809bd9a65eddac34588e8b66af
diagNOS-it <scott@diagnos.it.com>
2025-08-31 12:39:22 +1200

    Fixing config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const CONFIG = {
   companyName: "DiagNOS",
   email: "scott@diagnos.it.com",
   domain: "https://www.diagnos.it.com",
-  diagnosprivacyPolicyUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/diagNOS_policies/@main/policies/privacy_policy.html",
-  diagnostermsOfServiceUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/diagNOS_policies/@main/policies/terms_policy.html",
+  diagnosprivacyPolicyUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/diagNOS_policies@main/policies/privacy_policy.html",
+  diagnostermsOfServiceUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/diagNOS_policies@main/policies/terms_policy.html",
   privacyPolicyUrl: "https://cdn.jsdelivr.net/gh/diagNOS-it/Elephnt_Policies@main/policies/privacy_policy.html",
   mission: "To provide rapid diagnostic assistance offline, where ever you need it."
 };
